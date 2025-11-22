0dda6fb93c2ee4ed0e5afc75f56bf7c776456e4b
sali pc01 <thesalimft@gmail.com>
2025-11-22 07:33:28 -0800

    after post api, mobile menu icon, time counter

--- src/components/layout/MobileNavbar .jsx
+++ src/components/layout/MobileNavbar .jsx
@@ -1,23 +1,38 @@
 import { Link } from 'react-router-dom';
 
-function MobileNavbar () {
+function MobileNavbar() {
   return (
     <>
       <div className="mobile-navbar-area d-sm-block d-lg-none">
         <div>
-          <Link to="/store">Store</Link>
+          <Link to="/store">
+          <i class="bi bi-house-door-fill"></i>
+            {/* Store */}
+          </Link>
         </div>
         <div>
-          <Link to="/card">Cards</Link>
+          <Link to="/card">
+          <i class="bi bi-cart-fill"></i>
+            {/* Cards */}
+          </Link>
         </div>
         <div>
-          <Link to="/order">Orders</Link>
+          <Link to="/order">
+          <i class="bi bi-card-checklist"></i>
+            {/* Orders */}
+          </Link>
         </div>
         <div>
-          <Link to="/contact-us">Contact Us</Link>
+          <Link to="/contact-us">
+          <i class="bi bi-telephone-outbound-fill"></i>
+            {/* Contact Us */}
+          </Link>
         </div>
         <div>
-          <Link to="/about-us">About Us</Link>
+          <Link to="/about-us">
+          <i class="bi bi-person-bounding-box"></i>
+            {/* About Us */}
+          </Link>
         </div>
       </div>
     </>
--- src/pages/AboutUsPage.jsx
+++ src/pages/AboutUsPage.jsx
@@ -1,7 +1,32 @@
+import { useEffect, useState } from "react";
+
 export default function AboutUsPage() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((data) => setPosts(data))
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
-    <>
-      <div>AboutUs</div>
-    </>
-  )
+    <div className="container posts-area">
+      <div className="m-lg-5 m-sm-1">
+      <h2>All Posts</h2>
+
+      <div>
+        {posts.length === 0 && <p>Loading...</p>}
+
+        {posts.map((post) => (
+          <div key={post.id} style={{ marginBottom: "20px" }}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+            <hr />
+          </div>
+        ))}
+      </div>
+    </div>
+    </div>
+  );
 }
