5146ea46da4bf18f9357939bf18f05b3abaa5889
sali pc01 <thesalimft@gmail.com>
2025-11-29 09:24:17 -0800

    add login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,6 +1,28 @@
 
 export default function LoginPage() {
-  return (
-    <div>LoginPage</div>
-  )
+    return (
+        <div className="">
+            <div className="login-form">
+                <div className="">
+                    <form>
+                        <div>
+                            <label className="form-label" htmlFor="username">username</label>
+                            <input className="form-control" type="text" id="username" name="username" />
+                        </div>
+                        <div>
+                            <label className="form-label" htmlFor="Password">Password</label>
+                            <input className="form-control" type="text" id="Password" name="Password" />
+                        </div>
+                        <div>
+                            <label className="form-label" htmlFor="remember">remember</label>
+                            <input className="form-control" type="text" id="remember" name="remember" />
+                        </div>
+                        <div>
+                            <button type="submit" className="btn btn-blue">submit</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
 }
